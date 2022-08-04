aee8f8d1e491e32226eb40d3fb52b2cef9723dbb
Dzmitry_Melnikau1 <dzmitry_melnikau1@epam.com>
2022-08-04 14:42:34 +0200

    Home Task 5 add url for import lambda

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://2gaaedslz7.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://o97nlbjeai.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://2gaaedslz7.execute-api.eu-west-1.amazonaws.com/dev/',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
